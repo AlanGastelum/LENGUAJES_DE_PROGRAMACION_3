48f65dc947e2ef81779f72b9db3aa4cb64bbb151
AlanGastelum <oswaldo.9522@gmail.com>
2026-08-02 20:28:23 -0700

    Add files via upload

---
+++ Atividad_2/Clases/clsOperaciones.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_2.Clases
+{
+    internal class clsOperaciones
+    {
+        public int Sumar(int numero1, int numero2, int numero3, int numero4, int numero5, int numero6)
+        {
+            int resultado;
+            resultado = numero1 + numero2 + numero3 + numero4 + numero5 + numero6;
+            return resultado;
+        }
+        public int Restar(int numero1, int numero2, int numero3, int numero4, int numero5, int numero6)
+        {
+            int resultado;
+            resultado = numero1 - numero2 - numero3 - numero4 - numero5 - numero6;
+            return resultado;
+        }
+        public int Multiplicar(int numero1, int numero2, int numero3, int numero4, int numero5, int numero6)
+        {
+            int resultado;
+            resultado = numero1 * numero2 * numero3 * numero4 * numero5 * numero6;
+            return resultado;
+        }
+        public int Dividir(int numero1, int numero2, int numero3, int numero4, int numero5, int numero6)
+        {
+            int resultado;
+            resultado = numero1 / numero2 / numero3 / numero4 / numero5 / numero6;
+            return resultado;
+        }
+    }
+}
---
+++ Atividad_2/Clases/clsSaludo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_2.Clases
+{
+    public class clsSaludo
+    {
+        public string Saludar(string nombre)
+        {
+            string saludo;
+            saludo= "Hola " + nombre + ", que tengas un excelente día.";
+            return saludo;
+        }
+    }
+}
